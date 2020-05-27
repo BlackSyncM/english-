e62f4e2db7232b2f8cfd0b2c6417694579f9f8bf
WQMUSIC <mixa.mac2011@yandex.ru>
2020-05-27 12:24:42 +0700

    create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include "menu.h"
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    hello();
+    system("cls");
+    main_menu();
+    return 0;
+}
